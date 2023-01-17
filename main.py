e8acd635cce83ba9d6bb558a3d90e7a50777adf3
HermanRu <gshelopin@gmail.com>
2023-01-17 17:59:31 +0300

    separate solve_examples in to def, fix unique items in full_scan

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import sys
 import pandas as pd
 
 
-def menu():
+def menu(user):
     print('='*35)
     print('1 - User\n2 - Multiplication table scan\n3 - Mistakes and long answers')
     print('4 - Statistics\n5 - Exit')
@@ -15,14 +15,14 @@ def menu():
             your_choice = int(input('Make your choice... '))
             if your_choice == 1:
                 print('Not yet ;))')
-                menu()
+                menu(user)
             elif your_choice == 2:
-                full_scan()
+                full_scan(user)
             elif your_choice == 3:
-                repeat()
+                repeat(user)
             elif your_choice == 4:
                 print('Not yet ;))')
-                menu()
+                menu(user)
             elif your_choice == 5:
                 sys.exit()
             else:
@@ -31,16 +31,26 @@ def menu():
             print('Only numbers!!!')
 
 
-def full_scan():
+def full_scan(user):
     # make all examples
     all_examples = []
     for a in range(2, 10):
         for b in range(2, 10):
             all_examples.append([a, b])
-    all_examples = list(set(all_examples))
+    result = []
+    [result.append(x) for x in all_examples if x not in result]
+    solve_examples(user, result)
+
+
+def solve_examples(user, lst_examples):
+    last_session = cur.execute(f'SELECT max(session_id) FROM multiplication WHERE user_id = {user}').fetchone()[0]
+    if not isinstance(last_session, int):
+        last_session = 0
+    session_id = last_session + 1
+    all_examples = lst_examples
     random.shuffle(all_examples)
     # print(all_examples)
-    zzz = input('Ready?? Put the cursor to console and press ENTER... ')
+    zzz = input('Ready to the test?? press ENTER ... ')
     for example in all_examples:
         while True:
             try:
@@ -54,31 +64,13 @@ def full_scan():
                 break
             except:
                 print("Not correct input")
-        # print(example)
-        to_sql(user_id, session_id, example[0], example[1], example[2], example[3], example[4])
-    print_results()
+        to_sql(user, session_id, example[0], example[1], example[2], example[3], example[4])
+    print_results(user, session_id)
 
 
-def print_results():
-    results = cur.execute('''
-                            SELECT 
-                              time(SUM(duration), 'unixepoch') spent_time,
-                              COUNT(result) - sum(result) errors,
-                              COUNT(result) total,
-                              round(100.0*sum(result)/COUNT(result), 1) success_score,
-                              round(SUM(duration)/COUNT(result),1) avg_time,
-                            FROM multiplication m 
-                            WHERE user_id = (?) AND session_id = (?) 
-                          ''', [user_id, session_id]).fetchone()
-    print("Well Done!!! :))")
-    print(f'Time in game {results[0]},\nErrors: {results[1]}/{results[2]}, \nSuccess score: {results[3]}%')
-    print(f'Average Answer Time: {results[4]} sec')
-    menu()
-
-
-def repeat():
+def repeat(user):
     df = pd.read_sql_query(sql='SELECT * FROM multiplication', con=con)
-    df1 = df[df.user_id == user_id] \
+    df1 = df[df.user_id == user] \
         .groupby(["a", "b"], as_index=False) \
         .agg({'duration': 'mean', 'result': 'mean'}) \
         .sort_values(by='duration', ascending=False)
@@ -90,24 +82,24 @@ def repeat():
     for example in all_examples:
         print(f"{example[0]} * {example[1]} = {example[0] * example[1]}", end=" ")
         zzz = input("  ...remember and press ENTER  ")
-    random.shuffle(all_examples)
-    zzz = input('Ready to the test?? press ENTER  ')
-    for example in all_examples:
-        while True:
-            try:
-                start_time = datetime.datetime.now()
-                example.append(int(input(f"{example[0]} * {example[1]} = ")))
-                example.append((datetime.datetime.now() - start_time).total_seconds())
-                if example[0] * example[1] == example[2]:
-                    example.append(1)
-                else:
-                    example.append(0)
-                break
-            except:
-                print("Not correct input")
-        # print(example)
-        to_sql(user_id, session_id, example[0], example[1], example[2], example[3], example[4])
-    print_results()
+    solve_examples(user, all_examples)
+
+
+def print_results(user, session):
+    results = cur.execute('''
+                            SELECT 
+                              time(SUM(duration), 'unixepoch') spent_time,
+                              COUNT(result) - sum(result) errors,
+                              COUNT(result) total,
+                              round(100.0*sum(result)/COUNT(result), 1) success_score,
+                              round(SUM(duration)/COUNT(result),1) avg_time,
+                            FROM multiplication m 
+                            WHERE user_id = (?) AND session_id = (?) 
+                          ''', [user, session]).fetchone()
+    print("Well Done!!! :))")
+    print(f'Time in game {results[0]},\nErrors: {results[1]}/{results[2]}, \nSuccess score: {results[3]}%')
+    print(f'Average Answer Time: {results[4]} sec')
+    menu(user)
 
 
 def create_db():
@@ -130,11 +122,9 @@ def to_sql(us_id, ses_id, a, b, c, duration, result):
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     con = sqlite3.connect('multi_db.db')
     cur = con.cursor()
@@ -143,9 +133,5 @@ if __name__ == '__main__':
     username = 'Vita'
     print_hi(username)
     print('Put the cursor to console and press ENTER')
-    last_session = cur.execute(f'SELECT max(session_id) FROM multiplication WHERE user_id = {user_id}').fetchone()[0]
-    if not isinstance(last_session, int):
-        last_session = 0
-    session_id = last_session + 1
-    menu()
+    menu(user_id)
 
