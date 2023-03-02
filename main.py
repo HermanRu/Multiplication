bc85b0b5bd1d0b015b04c6b776ca367a44487296
HermanRu <gshelopin@gmail.com>
2023-03-03 00:10:05 +0300

    refactor menu

--- main.py
+++ main.py
@@ -12,136 +12,26 @@ def print_hi(name):
     print(f'Hi, {name}')
 
 
-def menu(user):
-    print('=' * 35)
-    menu_options = {
-        1: 'Choose User',
-        2: 'Crete New User',
-        3: 'Scan Multiplication table',
-        4: 'Mistakes and long answers',
-        5: 'Statistics for your sessions',
-        6: 'Winners',
-        0: 'Exit'
-    }
-
-    for key in menu_options.keys():
-        print(key, '--', menu_options[key])
-
-    while True:
-        option = ''
-        try:
-            option = int(input('Enter your choice: '))
-        except ValueError:
-            print('Wrong input. Please enter a number ...')
-        # Check what choice was entered
-        if option == 1:
-            # 1: 'Choose User'
-            df_names = pd.read_sql_query(sql='SELECT user_id, name FROM users', con=con)
-            print('Choose your name or "0" to create new user:')
-            print(tabulate(df_names, headers="keys", tablefmt='psql', showindex=False))
-            try:
-                global user_id
-                user_id = int(input('Input your id number... '))
-                user_name = cur.execute("SELECT name FROM users WHERE user_id =(?)", [user_id]).fetchone()[0]
-                print_hi(user_name)
-                menu(user_id)
-
-            except ValueError:
-                print('Wrong input. Please enter a number ...')
-            exit()
-
-        elif option == 2:
-            # 2: 'Crete New User'
-            user_name = input('Input your Name... ')
-            cur.execute("INSERT INTO users (name) VALUES (?)", [user_name])
-            con.commit()
-            print('Done!')
-            print_hi(user_name)
-            get_id = cur.execute("SELECT user_id FROM users WHERE name =(?)", [user_name]).fetchone()[0]
-            menu(get_id)
-
-        elif option == 3:
-            # 3: 'Scan Multiplication table'
-            full_scan(user)
-        elif option == 4:
-            # 4: 'Mistakes and long answers'
-            repeat(user)
-        elif option == 5:
-            # 5: 'Statistics'
-            query = f"""
-            SELECT
-                session_id,
-                COUNT(result) - SUM(result) wrong_answers,
-                SUM(result) correct_answers,
-                ROUND(AVG(duration),1) avg_answer_time
-            FROM multiplication
-            WHERE user_id = {user}
-            GROUP BY session_id
-            """
-            print('This is your results')
-            print(tabulate(pd.read_sql_query(sql=query, con=con), headers="keys", tablefmt='psql', showindex=False))
-            # try/except block
-            session = int(input('Input the number of session to see more details and heatmap: '))
-
-            last_session = cur.execute("""
-                        SELECT max(session_id) max_session_id
-                        FROM multiplication m 
-                        WHERE user_id = (?)
-                    """, [user]).fetchone()[0]
-
-            print_results(user, session)
-            print_hi(user_name)
-            menu(user)
-
-        elif option == 6:
-            query = """
-                    SELECT
-                        a.name Name,
-                        SUM(b.result) \"Solved examples\",
-                        COUNT(result) - SUM(result) \"Wrong answers\",
-                        SUM(result) \"Correct answers\",
-                        round(100.0*sum(result)/COUNT(result), 1) \"Success score\",
-                        ROUND(AVG(duration),1) \"Average answer time\"
-                    FROM 
-                        users a
-                        JOIN multiplication b on a.user_id = b.user_id
-                    GROUP BY a.name
-                    ORDER BY "Solved examples" DESC
-                    """
-            print(tabulate(pd.read_sql_query(sql=query, con=con), headers="keys", tablefmt='psql', showindex=False))
-            print_hi(user_name)
-            menu(user)
-
-        elif option == 0:
-            # 0: 'Exit'
-            print('Thanks you, come back again!')
-            exit()
-        else:
-            print('Invalid option. Please enter a number between 1 and 5.')
-
-
-def full_scan(user):
+def full_scan(u_id):
     # make all examples
     all_examples = []
     for a in range(2, 10):
         for b in range(2, 10):
             all_examples.append([a, b])
-    result = []
-    [result.append(x) for x in all_examples if x not in result]
-    solve_examples(user, result)
+    examples = []
+    [examples.append(x) for x in all_examples if x not in examples]
+    solve_examples(u_id, examples)
 
 
-def solve_examples(user, lst_examples):
-    last_session = cur.execute(f'SELECT max(session_id) FROM multiplication WHERE user_id = {user}').fetchone()[0]
+def solve_examples(u_id, list_examples):
+    last_session = cur.execute(f'SELECT max(session_id) FROM multiplication WHERE user_id = {u_id}').fetchone()[0]
     if not isinstance(last_session, int):
         last_session = 0
     session_id = last_session + 1
-    all_examples = lst_examples
-    random.shuffle(all_examples)
-    # print(all_examples)
+    random.shuffle(list_examples)
     zzz = input('Ready to the test?? press ENTER ... ')
-    n, n_all = 1, len(all_examples)
-    for example in all_examples:
+    n, n_all = 1, len(list_examples)
+    for example in list_examples:
         while True:
             try:
                 start_time = datetime.datetime.now()
@@ -155,15 +45,14 @@ def solve_examples(user, lst_examples):
                 break
             except ValueError:
                 print("Not correct input")
-        print('before sql query')
-        to_sql(user, session_id, example[0], example[1], example[2], example[3], example[4])
+        to_sql(u_id, session_id, example[0], example[1], example[2], example[3], example[4])
     print("Well Done!!! :))")
-    print_results(user, session_id)
+    print_results(u_id, session_id)
 
 
-def repeat(user):
+def repeat(u_id):
     df = pd.read_sql_query(sql='SELECT * FROM multiplication', con=con)
-    df1 = df[df.user_id == user] \
+    df1 = df[df.user_id == u_id] \
         .groupby(["a", "b"], as_index=False) \
         .agg({'duration': 'mean', 'result': 'mean'}) \
         .sort_values(by='duration', ascending=False)
@@ -175,10 +64,10 @@ def repeat(user):
     for example in all_examples:
         print(f"{example[0]} * {example[1]} = {example[0] * example[1]}", end=" ")
         zzz = input("  ...remember and press ENTER  ")
-    solve_examples(user, all_examples)
+    solve_examples(u_id, all_examples)
 
 
-def print_results(user, session):
+def print_results(u_id, session_id):
     results = cur.execute('''
                             SELECT 
                               time(SUM(duration), 'unixepoch') spent_time,
@@ -188,17 +77,16 @@ def print_results(user, session):
                               round(SUM(duration)/COUNT(result),1) avg_time
                             FROM multiplication m 
                             WHERE user_id = (?) AND session_id = (?) 
-                          ''', [user, session]).fetchone()
+                          ''', [u_id, session_id]).fetchone()
     print(f'Time in game {results[0]}')
     print(f'Errors: {results[1]}/{results[2]}')
     print(f'Success score: {results[3]}%')
     print(f'Average Answer Time: {results[4]} sec')
-    density_heatmap(f'SELECT * FROM multiplication WHERE user_id = {user} AND session_id = {session}')
-    menu(user)
+    density_heatmap(f'SELECT * FROM multiplication WHERE user_id = {u_id} AND session_id = {session_id}')
 
 
-def density_heatmap(query):
-    df = pd.read_sql_query(sql=query, con=con)
+def density_heatmap(sql_query):
+    df = pd.read_sql_query(sql=sql_query, con=con)
 
     fig1 = px.density_heatmap(df, x='a', y='b', z="duration", histfunc="avg",
                               nbinsx=9, nbinsy=9, text_auto='.1f', height=500, width=600)
@@ -245,7 +133,6 @@ def create_db():
     session_duration REAL,
     solved_examples INTEGER);
     """)
-
     con.commit()
 
 
@@ -260,7 +147,116 @@ if __name__ == '__main__':
     cur = con.cursor()
     create_db()
     user_id = 0
-    username = 'No Name'
-    print_hi(username)
+    user_name = 'No_Name'
+    print_hi(user_name)
     print('Put the cursor to console and choose user, press "1" ENTER')
-    menu(user_id)
+
+    option = ''
+    while True:
+        if user_id == 0 and option == '':
+            option = 1
+        elif user_id != 0 and option != '':
+            print('=' * 35)
+            menu_options = {
+                1: 'Choose User',
+                2: 'Crete New User',
+                3: 'Scan Multiplication table',
+                4: 'Mistakes and long answers',
+                5: 'Statistics for your sessions',
+                6: 'Winners',
+                0: 'Exit'
+            }
+            for key in menu_options.keys():
+                print(key, '--', menu_options[key])
+            try:
+                option = int(input('Enter your choice: '))
+            except ValueError:
+                print('Wrong input. Please enter a number ...')
+        # Check what choice was entered
+        if option == 1:
+            # 1: 'Choose User'
+            df_names = pd.read_sql_query(sql='SELECT user_id, name FROM users', con=con)
+            print('Choose your name or "0" to create new user:')
+            print(tabulate(df_names, headers="keys", tablefmt='psql', showindex=False))
+            try:
+                user_id = int(input('Input your id number... '))
+                if user_id == 0:
+                    print('NULL')
+                    option = 2
+                else:
+                    user_name = cur.execute("SELECT name FROM users WHERE user_id =(?)", [user_id]).fetchone()[0]
+                    print_hi(user_name)
+                # Добавить проверку на есть ли user_id в БД
+
+            except ValueError:
+                print('Wrong input. Please enter a number ...')
+            # exit()
+
+        elif option == 2:
+            # 2: 'Crete New User'
+            print('New User!')
+            user_name = input('Input your Name... ')
+            # Если ноль то выйти
+            cur.execute("INSERT INTO users (name) VALUES (?)", [user_name])
+            con.commit()
+            print('Done!')
+            print_hi(user_name)
+            user_id = cur.execute("SELECT user_id FROM users WHERE name =(?)", [user_name]).fetchone()[0]
+
+        elif option == 3:
+            # 3: 'Scan Multiplication table'
+            full_scan(user_id)
+        elif option == 4:
+            # 4: 'Mistakes and long answers'
+            repeat(user_id)
+        elif option == 5:
+            # 5: 'Statistics'
+            query = f"""
+                SELECT
+                    session_id,
+                    COUNT(result) - SUM(result) wrong_answers,
+                    SUM(result) correct_answers,
+                    ROUND(AVG(duration),1) avg_answer_time
+                FROM multiplication
+                WHERE user_id = {user_id}
+                GROUP BY session_id
+                """
+            print('This is your results')
+            print(tabulate(pd.read_sql_query(sql=query, con=con), headers="keys", tablefmt='psql', showindex=False))
+            # need to add try/except block
+            session = int(input('Input the number of session to see more details and heatmap: '))
+
+            # last_session = cur.execute("""
+            #                 SELECT max(session_id) max_session_id
+            #                 FROM multiplication m
+            #                 WHERE user_id = (?)
+            #             """, [user_id]).fetchone()[0]
+
+            print_results(user_id, session)
+            print_hi(user_name)
+
+        elif option == 6:
+            # 6:'Winners'
+            query = """
+                        SELECT
+                            a.name Name,
+                            SUM(b.result) \"Solved examples\",
+                            COUNT(result) - SUM(result) \"Wrong answers\",
+                            SUM(result) \"Correct answers\",
+                            round(100.0*sum(result)/COUNT(result), 1) \"Success score\",
+                            ROUND(AVG(duration),1) \"Average answer time\"
+                        FROM 
+                            users a
+                            JOIN multiplication b on a.user_id = b.user_id
+                        GROUP BY a.name
+                        ORDER BY "Solved examples" DESC
+                        """
+            print(tabulate(pd.read_sql_query(sql=query, con=con), headers="keys", tablefmt='psql', showindex=False))
+            print_hi(user_name)
+
+        elif option == 0:
+            # 0: 'Exit'
+            print(f'Thanks you {user_name}, come back again!')
+            exit()
+        else:
+            print('Invalid option. Please enter a number between 1 and 6. or 0 to exit')
